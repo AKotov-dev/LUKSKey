0629fca07314cd1026aee4d6ca05abe7a7b377bc
AKotov-dev <alex_q_2000@mail.ru>
2024-01-02 14:09:58 +0300

    v0.1 - initial release

---
+++ lukskey/backup/start_trd.pas
@@ -0,0 +1,138 @@
+unit start_trd;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, Process, SysUtils, ComCtrls, Forms;
+
+type
+  StartKeyChange = class(TThread)
+  private
+
+    { Private declarations }
+  protected
+  var
+    Result: TStringList;
+
+    procedure Execute; override;
+
+    procedure ShowLog;
+    procedure StartProgress;
+    procedure StopProgress;
+
+  end;
+
+implementation
+
+uses Unit1;
+
+{ TRD }
+
+procedure StartKeyChange.Execute;
+var
+  ExProcess: TProcess;
+begin
+  try //Вывод лога и прогресса
+    Synchronize(@StartProgress);
+
+    FreeOnTerminate := True; //Уничтожить по завершении
+    Result := TStringList.Create;
+
+    //Рабочий процесс
+    ExProcess := TProcess.Create(nil);
+
+    //Создаём раздел ${usb}1
+    ExProcess.Executable := 'bash';
+    ExProcess.Parameters.Add('-c');
+    //Группа команд (parted)
+    ExProcess.Parameters.Add(command);
+
+    ExProcess.Options := [poUsePipes, poStderrToOutPut];
+    //, poWaitOnExit (синхронный вывод)
+
+    ExProcess.Execute;
+
+    //Выводим лог динамически
+   { while ExProcess.Running do
+    begin
+      Result.LoadFromStream(ExProcess.Output);
+
+      //Выводим лог
+      if Result.Count <> 0 then
+        Synchronize(@ShowLog);
+    end;}
+    Result.LoadFromStream(ExProcess.Output);
+    Synchronize(@ShowLog);
+
+  finally
+    Synchronize(@StopProgress);
+    Result.Free;
+    ExProcess.Free;
+    Terminate;
+  end;
+end;
+
+{ БЛОК ОТОБРАЖЕНИЯ ЛОГА }
+
+//Старт индикатора
+procedure StartKeyChange.StartProgress;
+begin
+  with MainForm do
+  begin
+    LogMemo.Text := SWaitChangeKey;
+
+    ProgressBar1.Style := pbstMarquee;
+    ProgressBar1.Refresh;
+
+    ListBox1.Enabled := False;
+    Edit1.Enabled := False;
+    Edit2.Enabled := False;
+    ChangeBtn.Enabled := False;
+
+    Application.ProcessMessages;
+  end;
+end;
+
+//Стоп индикатора
+procedure StartKeyChange.StopProgress;
+begin
+  with MainForm do
+  begin
+    //    LogMemo.Append('');
+    LogMemo.Lines.Append(SCompleted);
+    ProgressBar1.Style := pbstNormal;
+    ProgressBar1.Refresh;
+
+    //Курсор в начало
+    LogMemo.SelStart := 0;
+    MainForm.LogMemo.SelLength := 0;
+
+    ListBox1.Enabled := True;
+    Edit1.Enabled := True;
+    Edit2.Enabled := True;
+    ChangeBtn.Enabled := True;
+
+    Application.ProcessMessages;
+  end;
+end;
+
+//Вывод лога
+procedure StartKeyChange.ShowLog;
+{var
+  i: integer;}
+begin
+  //Вывод построчно
+  // for i := 0 to Result.Count - 1 do
+  //   MainForm.LogMemo.Lines.Append(Result[i]);
+
+  //Вывод пачками
+  MainForm.LogMemo.Lines.Assign(Result);
+
+  //Промотать список вниз
+  MainForm.LogMemo.SelStart := Length(MainForm.LogMemo.Text);
+  MainForm.LogMemo.SelLength := 0;
+end;
+
+end.
---
+++ lukskey/unit1.pas
@@ -0,0 +1,184 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons,
+  ComCtrls, Process, DefaultTranslator, AsyncProcess, Types, LCLType;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    FindLUKSPartitions: TAsyncProcess;
+    CheckBox1: TCheckBox;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    LUKSMountPoint: TAsyncProcess;
+    ImageList1: TImageList;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    ListBox1: TListBox;
+    LogMemo: TMemo;
+    ProgressBar1: TProgressBar;
+    ChangeBtn: TSpeedButton;
+    StaticText1: TStaticText;
+    procedure FindLUKSPartitionsReadData(Sender: TObject);
+    procedure CheckBox1Change(Sender: TObject);
+    procedure LUKSMountPointReadData(Sender: TObject);
+    procedure FormKeyUp(Sender: TObject; var Key: word; Shift: TShiftState);
+    procedure FormShow(Sender: TObject);
+    procedure ListBox1Click(Sender: TObject);
+    procedure ListBox1DrawItem(Control: TWinControl; Index: integer;
+      ARect: TRect; State: TOwnerDrawState);
+    procedure ChangeBtnClick(Sender: TObject);
+
+  private
+
+  public
+
+  end;
+
+resourcestring
+  SStartKeyChange = 'Change the key of the selected partition?';
+  SWaitChangeKey = 'Changing key, please wait...';
+  SCompleted = 'Completed.';
+  SMountPoint = 'Mount point (crypt_):';
+
+
+var
+  MainForm: TMainForm;
+  command: string;
+
+implementation
+
+uses start_trd;
+
+{$R *.lfm}
+
+{ TMainForm }
+
+
+procedure TMainForm.FormShow(Sender: TObject);
+begin
+  MainForm.Caption := Application.Title;
+  //Показываем разделы LUKS
+  FindLUKSPartitions.Execute;
+end;
+
+//Вывод точки монтирования
+procedure TMainForm.ListBox1Click(Sender: TObject);
+begin
+  if ListBox1.SelCount <> 0 then
+  begin
+    LUKSMountPoint.Parameters.Clear;
+    LUKSMountPoint.Parameters.Add('-c');
+    LUKSMountPoint.Parameters.Add(
+      'lsblk -l ' + ListBox1.Items[ListBox1.ItemIndex]);
+    // 'lsblk -l ' + ListBox1.Items[ListBox1.ItemIndex] + ' | grep crypt');
+    // | awk ' + '''' + '{print $NF}' + '''' + ')"');
+    LUKSMountPoint.Execute;
+  end;
+end;
+
+//Значки в списке разделов LUKS
+procedure TMainForm.ListBox1DrawItem(Control: TWinControl; Index: integer;
+  ARect: TRect; State: TOwnerDrawState);
+
+var
+  BitMap: TBitMap;
+begin
+  try
+    BitMap := TBitMap.Create;
+    with ListBox1 do
+    begin
+      Canvas.FillRect(aRect);
+
+      //Название (текст по центру-вертикали)
+      Canvas.TextOut(aRect.Left + 32, aRect.Top + ItemHeight div
+        2 - Canvas.TextHeight('A') div 2 + 1, Items[Index]);
+
+      //Иконки
+      ImageList1.GetBitMap(0, BitMap);
+
+      Canvas.Draw(aRect.Left + 2, aRect.Top + (ItemHeight - 24) div 2 + 1, BitMap);
+    end;
+  finally
+    BitMap.Free;
+  end;
+end;
+
+
+//Создание файлов ключей и запуск команды с выводом лога
+procedure TMainForm.ChangeBtnClick(Sender: TObject);
+var
+  S: TStringList;
+  FStartKeyChange: TThread;
+begin
+  if (ListBox1.SelCount = 0) or (Trim(Edit1.Text) = '') or (Trim(Edit2.Text) = '') then
+    Exit;
+
+  try
+    S := TStringList.Create;
+
+    if not DirectoryExists('/root/tmp') then MkDir('/root/tmp');
+
+    S.Text := Edit1.Text;
+    S.SaveToFile('/root/tmp/luksold.key');
+
+    S.Text := Edit2.Text;
+    S.SaveToFile('/root/tmp/luksnew.key');
+
+    command :=
+      'truncate -s -1 /root/tmp/{luksold.key,luksnew.key}; ' +
+      'cryptsetup luksChangeKey ' + ListBox1.Items[ListBox1.ItemIndex] +
+      ' --key-file /root/tmp/luksold.key /root/tmp/luksnew.key; ' +
+      'shred -n 3 -u -z /root/tmp/{luksold.key,luksnew.key}';
+
+    if MessageDlg(SStartKeyChange, mtConfirmation, [mbYes, mbNo], 0) = mrYes then
+    begin
+      FStartKeyChange := StartKeyChange.Create(False);
+      FStartKeyChange.Priority := tpNormal;
+    end;
+  finally
+    S.Free;
+  end;
+end;
+
+procedure TMainForm.FormKeyUp(Sender: TObject; var Key: word; Shift: TShiftState);
+begin
+  if Key = VK_RETURN then ChangeBtn.Click;
+end;
+
+//Показать/Скрыть пароли
+procedure TMainForm.CheckBox1Change(Sender: TObject);
+begin
+  if CheckBox1.Checked then
+  begin
+    Edit1.PasswordChar := #0;
+    Edit2.PasswordChar := #0;
+  end
+  else
+  begin
+    Edit1.PasswordChar := '*';
+    Edit2.PasswordChar := '*';
+  end;
+end;
+
+//Показать UUID
+procedure TMainForm.LUKSMountPointReadData(Sender: TObject);
+begin
+  LogMemo.Lines.LoadFromStream(LUKSMountPoint.Output);
+end;
+
+//Читаем список разделов LUKS
+procedure TMainForm.FindLUKSPartitionsReadData(Sender: TObject);
+begin
+  ListBox1.Items.LoadFromStream(FindLUKSPartitions.Output);
+end;
+
+end.
